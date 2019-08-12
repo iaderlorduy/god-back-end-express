ffb110afd5d13c533faa0c0d1cd77761beb4d0a5
iaderlorduy <iaderlor@gmail.com>
2019-08-12 03:27:34 -0500

    add express test

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
-var http = require('http');
+//import gameRouter from './routes/gameRouter';
 
-http.createServer(function (req, res) {
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, Azure!');
+const app = express();
 
-}).listen(process.env.PORT || 8080);
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+// eslint-disable-next-line no-unused-vars
+const db = mongoose.connect('mongodb://root:abc123@ds231242.mlab.com:31242/god', {
+    useNewUrlParser: true,
+});
+
+const port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//app.use('/api', gameRouter);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!');
+});
+
+// agregar middleware para el manejo de las excepciones
+
+app.server = app.listen(port, () => {
+    // console.log(`Running on port ${port}`);
+});
+
+module.exports = app;

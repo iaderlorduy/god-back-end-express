e540f65421da3af445f7ddd08bcc3f93f749c514
iaderlorduy <iaderlor@gmail.com>
2019-08-12 02:24:46 -0500

    test azure

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'development') {
     useNewUrlParser: true,
   });
 }
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
